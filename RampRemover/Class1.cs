52e8fc74d155b191d1e943c864cb9088e112a198
belowjobsrock <62730919+belowjobsrock@users.noreply.github.com>
2022-04-16 18:25:48 -0400

    Add project files.

---
+++ RampRemover/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using MelonLoader;
+using UnityEngine;
+
+
+public class noRamps : MelonMod
+{
+
+	public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+	{
+		if (sceneName == "Elysium")
+		{
+
+            (from x in Resources.FindObjectsOfTypeAll<GameObject>()
+			 where x.name.Equals("Jump Takeoff - 05 Large")
+			 select x).ToList().ForEach(delegate (GameObject x)
+			 {
+				 
+				 UnityEngine.Object.Destroy(x);
+             });
+			return;
+		}
+
+	}
+
+}
